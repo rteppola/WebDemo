fcb41ef3db5a2af46f51ceffdbc4f91c8bb38a87
Reijo Teppola <reijo.teppola@gmail.com>
2019-03-06 13:02:11 +0200

    TestiApi- ja AsiakkaatApikontrollerit lisätty.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/AsiakkaatApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreBackend.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+   // [Route("api/[controller]")]
+    [Route("api/asiakkaat")]
+    [ApiController]
+    public class AsiakkaatApiController : ControllerBase
+    {
+        [Route("listaus")] // ei case sensitiivinen
+        public List<Customers> Listaus()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> allCustomers = context.Customers.ToList();
+
+            return allCustomers;
+        }
+
+        [Route("yksittäinen/{asiakasId}")] // ei case sensitiivinen
+        public Customers Yksittäinen(string asiakasId)
+        {
+            NorthwindContext context = new NorthwindContext();
+            Customers asiakas = context.Customers.Find(asiakasId);
+
+            //LINQ
+            //Customers asiakas2 = (from c in context.Customers
+            //                      where c.CustomerId == asiakasId
+            //                      select c).FirstOrDefault();
+
+            return asiakas;
+        }
+    }
+}
---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/TestiApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreBackend.Models; //tarvitaan Auto mallia varten
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/[controller]")] // = api/TestiApi
+    [ApiController]
+    public class TestiApiController : ControllerBase
+    {   
+        [Route("Lista")]    // <web-osoite>/api/TestiApi/Lista
+        public List<Auto> AutoLista()
+        {
+            List<Auto> autot = new List<Auto>()
+            {
+                new Auto()
+                {
+                    Merkki = "Ford",
+                    Malli = "Focus",
+                    Huippunopeus = 190
+                },
+                new Auto()
+                {
+                    Merkki = "Toyota",
+                    Malli = "Avensis",
+                    Huippunopeus = 220
+                },
+                new Auto()
+                {
+                    Merkki = "Porsche",
+                    Malli = "911",
+                    Huippunopeus = 300
+                }
+            };
+            return autot;
+        }
+
+    }
+}
