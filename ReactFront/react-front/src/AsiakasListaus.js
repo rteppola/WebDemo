2e1e5bcf6b58c289aa0f5adad8a7b03c5da06260
Reijo Teppola <reijo.teppola@gmail.com>
2019-03-07 13:36:12 +0200

    Aloitettu AsiakasListaus React-komponentin teko.

---
+++ ReactFront/react-front/src/AsiakasListaus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AsiakasListaus extends Component {
+  render() {
+    return (
+        <div>
+           
+        </div>
+    );
+  }
+}
+
+export default AsiakasListaus;	
